4a982caa74e7f9208b63138a8e871e83b0873817
Ikeya <yuumake@gmail.com>
2021-12-11 17:13:34 +1100

    setting up firebase

---
+++ Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB4XoUksoHeOh6sIdoROvFxZYLdT6m0Ogg",
+    authDomain: "superly-v2.firebaseapp.com",
+    projectId: "superly-v2",
+    storageBucket: "superly-v2.appspot.com",
+    messagingSenderId: "667176211668",
+    appId: "1:667176211668:web:9daea3ff94026a668ad741",
+    measurementId: "G-HSQ1THDTXC"
+  };
